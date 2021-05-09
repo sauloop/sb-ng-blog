48dd136d0e83468c5b5c157c31b7e8e6ab8f768e
Pablo Giraldo <pablogirald@yahoo.es>
2021-05-09 13:51:00 +0200

    Add response dto

--- src/main/java/com/inverenlace/sbngblog/controller/ArticleRestController.java
+++ src/main/java/com/inverenlace/sbngblog/controller/ArticleRestController.java
@@ -1,6 +1,8 @@
 package com.inverenlace.sbngblog.controller;
 
+import com.inverenlace.sbngblog.converter.ArticleConverter;
 import com.inverenlace.sbngblog.dto.ArticleDto;
+import com.inverenlace.sbngblog.dto.ResponseDto;
 import com.inverenlace.sbngblog.entity.Article;
 import com.inverenlace.sbngblog.entity.Category;
 import com.inverenlace.sbngblog.service.ArticleService;
@@ -14,10 +16,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "*")
 @RequestMapping(value = "/api/v1/articles", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ArticleRestController {
@@ -28,26 +34,51 @@ public class ArticleRestController {
     @Value("${pagesize}")
     private int pagesize;
 
+
     @GetMapping
-    public ResponseEntity<Page<Article>> findAllArticles(@RequestParam(defaultValue = "0") int page,
-                                                         @RequestParam(name = "categoryId", required = false) Long categoryId) {
-        Pageable pageable = PageRequest.of(page, this.pagesize, Sort.by("id").descending());
-        Page<Article> articles = articleService.findAllArticles(pageable);
-        if (categoryId == null) {
-            if (articles.isEmpty()) {
-//                return ResponseEntity.noContent().build();
-                return ResponseEntity.ok(articles);
+    public ResponseEntity<ResponseDto> findAllArticles(@RequestParam(defaultValue = "0") int page,
+                                                       @RequestParam(name = "categoryId", required = false) Long categoryId) {
+
+        final ResponseEntity<ResponseDto> responseEntity;
+
+        try {
+            Pageable pageable = PageRequest.of(page, this.pagesize, Sort.by("id").descending());
+            Page<Article> articles = articleService.findAllArticles(pageable);
+
+            if (categoryId != null) {
+                articles = articleService.findByCategory(Category.builder().id(categoryId).build(), pageable);
             }
-        } else {
-            pageable = PageRequest.of(page, this.pagesize, Sort.by("id").descending());
-            articles = articleService.findByCategory(Category.builder().id(categoryId).build(), pageable);
-            if (articles.isEmpty()) {
-//                return ResponseEntity.notFound().build();
-                return ResponseEntity.ok(articles);
-            }
+
+            responseEntity = createResponseDto(articles);
+
+        } catch (final Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return ResponseEntity.ok(articles);
+
+        return responseEntity;
     }
+
+
+//    @GetMapping
+//    public ResponseEntity<Page<Article>> findAllArticles(@RequestParam(defaultValue = "0") int page,
+//                                                         @RequestParam(name = "categoryId", required = false) Long categoryId) {
+//        Pageable pageable = PageRequest.of(page, this.pagesize, Sort.by("id").descending());
+//        Page<Article> articles = articleService.findAllArticles(pageable);
+//        if (categoryId == null) {
+//            if (articles.isEmpty()) {
+//                return ResponseEntity.noContent().build();
+//                return ResponseEntity.ok(articles);
+//            }
+//        } else {
+//            pageable = PageRequest.of(page, this.pagesize, Sort.by("id").descending());
+//            articles = articleService.findByCategory(Category.builder().id(categoryId).build(), pageable);
+//            if (articles.isEmpty()) {
+//                return ResponseEntity.notFound().build();
+//                return ResponseEntity.ok(articles);
+//            }
+//        }
+//        return ResponseEntity.ok(articles);
+//    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<ArticleDto> findArticleDtoById(@PathVariable Long id) {
@@ -77,5 +108,20 @@ public class ArticleRestController {
     public ResponseEntity<Void> deleteArticleById(@PathVariable Long id) {
         articleService.deleteArticleById(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    private ResponseEntity<ResponseDto> createResponseDto(final Page<Article> articlesPage) {
+        final List<ArticleDto> articlesDto = articlesPage.getContent().stream().map(article -> ArticleConverter.articleToArticleDto(article))
+                .collect(Collectors.toList());
+        final ResponseEntity<ResponseDto> responseEntity;
+        if (CollectionUtils.isEmpty(articlesDto)) {
+            responseEntity = new ResponseEntity<>(ResponseDto.create(Collections.emptyList(), 0, 0, 0, null, null),
+                    HttpStatus.OK);
+        } else {
+            responseEntity = new ResponseEntity<>(ResponseDto.create(articlesDto, (int) articlesPage.getTotalElements(),
+                    articlesPage.getTotalPages(), articlesPage.getNumber(), articlesPage.isFirst(),
+                    articlesPage.isLast()), HttpStatus.OK);
+        }
+        return responseEntity;
     }
 }
